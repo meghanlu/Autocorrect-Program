c4e5dd66f37a71e11063bbb723f3e74a585e31d3
meghanlu <lumeghan@gmail.com>
2021-04-20 21:05:37 -0500

    Created Dictionary test class

---
+++ tests/dictionary_test.cpp
@@ -0,0 +1,12 @@
+#include <catch2/catch.hpp>
+#include <core/dictionary.h>
+
+using autocorrect::Dictionary;
+using std::vector;
+using std::string;
+
+TEST_CASE("Test Is Word In Dictionary") {
+
+
+
+}
