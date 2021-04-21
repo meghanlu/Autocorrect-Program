9fcd4e90ac510b6b741a994e83dadc8f8ae45318
meghanlu <lumeghan@gmail.com>
2021-04-20 20:52:45 -0500

    Overrode operator>> to read from file

---
+++ src/core/word_list.cpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "core/word_list.h"
+
+namespace autocorrect {
+
+    WordList::WordList(bool is_alphabetized) : is_alphabetized_(is_alphabetized){}
+
+    WordList::WordList(vector<string> const& word_vec, bool is_alphabetized) :
+                         words_(word_vec), is_alphabetized_(is_alphabetized){
+        AlphabetizeWords();
+    }
+
+    std::istream &operator>>(std::istream &is, WordList &word_list) {
+        // Clear previous word list
+        word_list.words_.clear();
+        size_t length_of_longest_english_word = 200;
+        char word[200];
+        // Loop through each line
+        while (is.getline(word, length_of_longest_english_word)) {
+            word_list.words_.push_back(word);
+        }
+        word_list.AlphabetizeWords();
+        return is;
+    }
+
+
+} // namespace autocorrect
