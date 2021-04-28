ad91682415d52de2258c0bf120636b9a83c33dcc
meghanlu <lumeghan@gmail.com>
2021-04-27 23:50:44 -0500

    Created test case outlines

---
+++ tests/test_autocorrect.cpp
@@ -0,0 +1,12 @@
+#include <catch2/catch.hpp>
+#include <core/autocorrect.h>
+
+using autocorrect::Autocorrect;
+using std::vector;
+using std::string;
+
+TEST_CASE("Test Get Corrections") {
+
+
+
+}
