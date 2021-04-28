f888d41f12fd2f738a50839e2223d056c0017b5b
meghanlu <lumeghan@gmail.com>
2021-04-27 23:16:24 -0500

    Added test case for converting string to lowercase

--- tests/test_word_list.cpp
+++ tests/test_word_list.cpp
@@ -5,10 +5,26 @@ using autocorrect::WordList;
 using std::vector;
 using std::string;
 
+
+
 TEST_CASE("Test Alphabetizing") {
     WordList word_list(vector<string>{"zealous", "zeal", "aa", "aaa", "b"},
-                                                                false);
+                       false);
     REQUIRE(word_list.GetWordVector() == vector<string>{"aa", "aaa", "b",
-                                              "false", "zeal", "zealous"});
+                                                        "false", "zeal", "zealous"});
 
+}
+
+TEST_CASE("Test Convert String to Lowercase") {
+    SECTION("Lowercase String") {
+        REQUIRE(WordList::ConvertStringToLowercase("h e l lo") == "h e l lo");
+    }
+
+    SECTION("Capital String") {
+        REQUIRE(WordList::ConvertStringToLowercase("HEL LO") == "hel lo");
+    }
+
+    SECTION("Mixed Case String") {
+        REQUIRE(WordList::ConvertStringToLowercase("HeLlO") == "hello");
+    }
 }
