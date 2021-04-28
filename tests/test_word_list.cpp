b11b7e5ee58ebc97677243cee45687ec944a3593
meghanlu <lumeghan@gmail.com>
2021-04-27 23:38:06 -0500

    Added test case for frequency word list operator>>

--- tests/test_word_list.cpp
+++ tests/test_word_list.cpp
@@ -1,11 +1,33 @@
 #include <catch2/catch.hpp>
 #include <core/word_list.h>
+#include <fstream>
 
 using autocorrect::WordList;
 using std::vector;
 using std::string;
+using std::unordered_map;
 
-TEST_CASE("Test Constructors and Contains Frequencies") {
+TEST_CASE("Test operator>>") {
+    SECTION("Frequency Word List") {
+        std::ifstream ifs("../../../data/testing_word_frequencies.txt");
+        WordList frequencyWordList(false, true);
+        ifs >> frequencyWordList;
+
+        vector<string> expected_word_vector = {"meghan", "test", "testing"};
+        REQUIRE(frequencyWordList.GetWordVector() == expected_word_vector);
+
+        unordered_map<string, double>  expected_freq_map;
+        for (size_t i = 1; i < 4; i++) {
+            expected_freq_map.insert(
+                    std::make_pair(expected_word_vector.at(i), i));
+        }
+        REQUIRE(frequencyWordList.GetWordFrequencies() == expected_freq_map);
+    }
+
+
+}
+
+TEST_CASE("Test Contains Frequencies In Constructors") {
     SECTION("Empty Constructor") {
         WordList word_list;
         REQUIRE_FALSE(word_list.ContainsFrequencies());
