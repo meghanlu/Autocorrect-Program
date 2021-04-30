b87a442e6dddd0f64e13c769fb41c84be21cdbdf
meghanlu <lumeghan@gmail.com>
2021-04-29 23:53:17 -0500

    Overrode operator<<

--- src/core/autocorrect.cpp
+++ src/core/autocorrect.cpp
@@ -2,11 +2,11 @@
 namespace autocorrect {
 
     Autocorrect::Autocorrect(Dictionary const& dictionary)
-                                        : dictionary_(dictionary),
-                                        sym_spell(dictionary){
+                                        : dictionary_(dictionary){
+        sym_spell.SetDictionary(dictionary);
     }
 
-    std::map<std::string, std::vector<std::string>> Autocorrect::GiveFeedback(string const& string) {
+    std::map<std::string, std::vector<std::string>> Autocorrect::GetCorrections(string const& string) {
         std::istringstream iss(string);
         std::vector<std::string> words;
         std::map<std::string, std::vector<std::string>> suggestions;
@@ -18,14 +18,37 @@ namespace autocorrect {
         }
 
         for (std::string const& word : words) {
-            if (!dictionary_.IsWordInDictionary(word)) {
+            if (!dictionary_.IsWordInDictionary(WordList::ConvertStringToLowercase(word))) {
+                std::vector<std::string> suggested_words =
+                        sym_spell.GetSimilarWords(word, dictionary_);
                 suggestions.insert(std::pair<std::string,
                                    std::vector<std::string>>(word,
-                                           sym_spell.GetSimilarWords(word, dictionary_)));
+                                           suggested_words));
             }
         }
         return suggestions;
     }
 
+
+
+
+    std::ostream &operator<<(std::ostream &os, const Autocorrect &autocorrect) {
+
+        os.flush();
+        // SAVE DICTIONARY FREQUENCY MAP
+        for (auto const& word_freq_pair : autocorrect.dictionary_.GetFrequencies()) {
+            os << "* " << word_freq_pair.first << " ";
+            os << word_freq_pair.second << std::endl;
+        }
+        // SAVE SYMSPELL PRECALCULATED DELETES
+        for (auto const& delete_pair : autocorrect.sym_spell.GetPrecalculatedDeletes()) {
+            os << "& " << delete_pair.first << std::endl;
+            for (string const& associated_string : delete_pair.second) {
+                os << associated_string << std::endl;
+            }
+            os << "%" << std::endl;
+        }
+        return os;
+    }
 
 } // namespace autocorrect
