4d23c9fc29dfe48223b9c2dec86923cfbe5a3050
meghanlu <lumeghan@gmail.com>
2021-05-04 23:11:33 -0500

    Overrode keyDown function

--- src/visualizer/autocorrect_app.cpp
+++ src/visualizer/autocorrect_app.cpp
@@ -34,6 +34,19 @@ AutocorrectApp::AutocorrectApp() {
         }
     }
 
+    void AutocorrectApp::keyDown(ci::app::KeyEvent event) {
+        if (event.getCode() == ci::app::KeyEvent::KEY_RETURN) {
+            is_edit_mode_ = !is_edit_mode_;
+            if (!is_edit_mode_) {
+                correction_mode_.SetTextAndCorrections(textBox,
+                                                       autocorrect_.GetCorrections(textBox.getText()));
+            } else {
+                editMode.SetTextString(textBox.getText());
+            }
+        } else if (is_edit_mode_) {
+            editMode.HandleKeyDownEvent(event);
+        }
+    }
 
 
 
