fbe5082d0b0b0f6186e5b4629ffee0e8ace1b473
meghanlu <lumeghan@gmail.com>
2021-05-04 22:37:57 -0500

    Added GetWordToCorrect getter function

--- src/visualizer/correction_box.cpp
+++ src/visualizer/correction_box.cpp
@@ -30,55 +30,56 @@ void CorrectionBox::InitializeCorrections(std::pair<std::string, size_t> const& 
     contains_corrections_ = true;
 }
 
-    void CorrectionBox::Draw() {
-        // Draw solid box color
-        ci::gl::color(ci::Color("white"));
-        ci::gl::drawSolidRect(ci::Rectf(kTopLeftCoordinates,
-                                        kBottomRightCoordinates));
-        ci::gl::color(ci::Color("black"));
-        if (correction_rectangles_.empty()) {
-            ci::gl::drawStrokedRect(ci::Rectf(kTopLeftCoordinates,
-                                              kBottomRightCoordinates));
-            ci::gl::drawStringCentered("Sorry, we could not find suggestions",
-                                       ci::Rectf(kTopLeftCoordinates,
-                                                 kBottomRightCoordinates).getCenter(),
+void CorrectionBox::Draw() {
+    // Draw solid box color and outline
+    ci::gl::color(ci::Color("white"));
+    ci::gl::drawSolidRect(ci::Rectf(kTopLeftCoordinates,
+                                    kBottomRightCoordinates));
+    ci::gl::color(ci::Color("black"));
+    if (correction_rectangles_.empty()) {
+        ci::gl::drawStrokedRect(ci::Rectf(kTopLeftCoordinates,
+                                          kBottomRightCoordinates));
+        ci::gl::drawStringCentered("Sorry, we could not find suggestions",
+                                   ci::Rectf(kTopLeftCoordinates,
+                                    kBottomRightCoordinates).getCenter(),
+                                   ci::Color("black"),
+                                   ci::Font("Times New Roman", 30));
+    } else {
+        // Draw each individual correction box
+        for (auto const& correction_rectangle : correction_rectangles_) {
+            ci::gl::drawStrokedRect(correction_rectangle.second);
+            ci::gl::drawStringCentered(correction_rectangle.first,
+                                       correction_rectangle.second.getCenter(),
                                        ci::Color("black"),
                                        ci::Font("Times New Roman", 30));
-        } else {
-            // Draw each individual correction box
-            for (auto const& correction_rectangle : correction_rectangles_) {
-                ci::gl::drawStrokedRect(correction_rectangle.second);
-                ci::gl::drawStringCentered(correction_rectangle.first,
-                                           correction_rectangle.second.getCenter(),
-                                           ci::Color("black"),
-                                           ci::Font("Times New Roman", 30));
-            }
         }
-        ci::gl::color(ci::Color("white"));
     }
+    ci::gl::color(ci::Color("white"));
+}
 
-    std::string CorrectionBox::GetClickedCorrection(vec2 const& click_position) {
-        std::string clicked_string = GetClickedString(click_position);
-        if (!clicked_string.empty()) ClearSavedCorrections();
-        return clicked_string;
-    }
+std::string CorrectionBox::GetClickedCorrection(vec2 const& click_position) {
+    std::string clicked_string = GetClickedString(click_position);
+    if (!clicked_string.empty()) ClearSavedCorrections();
+    return clicked_string;
+}
 
-    std::string CorrectionBox::GetClickedString(vec2 const& position) {
-        for (auto const& correction_rectangle : correction_rectangles_) {
-            if (correction_rectangle.second.contains(position)) {
-                return correction_rectangle.first;
-            }
+std::string CorrectionBox::GetClickedString(vec2 const& position) {
+    for (auto const& correction_rectangle : correction_rectangles_) {
+        if (correction_rectangle.second.contains(position)) {
+            return correction_rectangle.first;
         }
-        return "";
     }
+    return "";
+}
 
-    void CorrectionBox::ClearSavedCorrections() {
-        correction_rectangles_.clear();
-        contains_corrections_ = false;
-    }
+void CorrectionBox::ClearSavedCorrections() {
+    correction_rectangles_.clear();
+    contains_corrections_ = false;
+}
 
-
-
+std::pair<std::string, size_t> CorrectionBox::GetWordToCorrect() {
+    return word_to_correct_;
+}
 
 }  // namespace visualizer
 
