23810fed7c9a42526dac2beccbd366f903845686
meghanlu <lumeghan@gmail.com>
2021-05-04 23:22:14 -0500

    Added comments

--- include/core/dictionary.h
+++ include/core/dictionary.h
@@ -26,11 +26,14 @@ namespace autocorrect {
         bool IsWordInDictionary(const string& word) const;
         double GetWordFrequency(string const& word) const;
         vector<string> GetWordVector() const;
+
+        /**
+         * Returns if the dictionary contains frequencies.
+         */
         bool ContainsFrequencies() const {return !word_frequencies_.empty();};
         unordered_map<string, double> GetFrequencies() const;
         void AddFrequencies(unordered_map<string, double> const& frequencies);
         void AddWords(vector<string> const& words_vector_);
-
         void SetWordFrequencies(const unordered_map<string, double> &frequencies);
 
     private:
